82be7f9624353bb03e1aa83068a0eb1210a6451b
overwriter <9856mmm@gmail.com>
2025-07-19 10:16:57 +0800

    fix: compile

--- Test/TestTool/testtool.cpp
+++ Test/TestTool/testtool.cpp
@@ -4,7 +4,7 @@
 
 const static QString token = "";
 const static QString api_base_url = "";
-Q_GLOBAL_STATIC(QOpenAI, ai, token, "QOpenAI", api_base_url);
+Q_GLOBAL_STATIC_WITH_ARGS(QOpenAI, ai, (token, "QOpenAI", api_base_url));
 
 QOpenAI *aiInstance()
 {
